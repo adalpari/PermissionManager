8b63f0aecd1d05e0ed7be9baba5b20ce4acc5aef
adalpari <adalpari@gmail.com>
2016-11-17 13:02:35 +0100

    onDetach()

--- pemissionmanager/src/main/java/com/github/adalpari/pemissionmanager/PermissionManager.java
+++ pemissionmanager/src/main/java/com/github/adalpari/pemissionmanager/PermissionManager.java
@@ -49,6 +49,10 @@ public class PermissionManager {
         this.listener = listener;
     }
 
+    public void onDetach() {
+        this.listener = null;
+    }
+
     /**
      *  Public methods to ask for permissions
      */
@@ -245,7 +249,6 @@ public class PermissionManager {
      *  It shold be called in Actitity's onRequestPermissionsResult
      */
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-
         // If request is cancelled, the result arrays are empty.
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             // permission was granted
